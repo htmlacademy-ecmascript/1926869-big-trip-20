1600590cf301027857d8ee062f66d662987c02c9
Виктор Филин <filinn14@gmail.com>
2023-06-06 08:42:42 +0300

    Шаблонизируй это

---
+++ src/model/points-model.js
@@ -0,0 +1,7 @@
+import { points } from '../mock/points.js';
+
+export default class PointsModel {
+  get points() {
+    return points;
+  }
+}
--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,10 +1,18 @@
 import { render } from '../framework/render.js';
-import CreateTripList from '../view/list-view.js';
-import CreateSort from '../view/sort-view.js';
-import EditTripPoint from '../view/edit-form-view.js';
-import CreatePoint from '../view/point-view.js';
+import CreateTripList from '../view/body/list-view.js';
+import CreateSort from '../view/body/sort-view.js';
+import EditTripPoint from '../view/body/edit-form-view.js';
+import CreatePoint from '../view/body/point-view.js';
+import { getRandomArrayElement } from '../utils.js';
 
 export default class BoardPresenter {
+  constructor({ pointsModel, offersModel, destinationsModel }) {
+    this.points = [...pointsModel.points];
+    this.offers = { ...offersModel.offers };
+    this.offersTypes = [...offersModel.offersTypes];
+    this.destinations = [...destinationsModel.destinations];
+  }
+
 
   tripBoardList = document.querySelector('.trip-events');
 
@@ -12,11 +20,18 @@ export default class BoardPresenter {
   init() {
     render(new CreateSort(), this.tripBoardList); // сортировка
     render(this.createTripList, this.tripBoardList); // создает список <ul>
-    render(new EditTripPoint(), this.createTripList.element);
 
-    for (let i = 0; i < 3; i++) {
-      render(new CreatePoint(), this.createTripList.element);
-    }
+    const pointForEditForm = getRandomArrayElement(this.points);
+    render(new EditTripPoint(
+      pointForEditForm,
+      this.offers[pointForEditForm.type],
+      this.offersTypes,
+      this.destinations
+    ), this.createTripList.element);
+    this.points.forEach((point) => {
+      const offers = this.offers[point.type];
+      render(new CreatePoint(point, offers), this.createTripList.element);
+    });
   }
 
 }
