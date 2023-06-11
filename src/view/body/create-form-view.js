63a104a2607e9ae69109b5cc4aadcb3f4dc36e99
Виктор Филин <filinn14@gmail.com>
2023-06-11 14:22:13 +0300

    шаблонизируй то

--- src/view/body/create-form-view.js
+++ src/view/body/create-form-view.js
@@ -1,6 +1,7 @@
 import AbstractView from '../../framework/view/abstract-view.js';
 
 function createCreateTripPointFormTemplate() {
+
   return (
     `<li class="trip-events__item">
       <form class="event event--edit" action="#" method="post">
